074763c5497cba6d2f8957e19d239b8c9a15d213
nollive <olivier.gaufres@gmail.com>
2024-07-30 16:50:12 +0200

    TEMP: files for interventions-analysis simulation (using IP cluster)

---
+++ R/nodscov2/launch-intervention-analysis.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=olivier.gaufres@pasteur.fr
+#SBATCH --output=/pasteur/appa/homes/ogaufres/logs/intervention-analysis/slurm-%j.out
+#SBATCH --error=/pasteur/appa/homes/ogaufres/logs/intervention-analysis/slurm-%j.err
+
+module purge
+module load R/4.4.0
+
+
+n_sim=$1
+list_beta=("0" "1/6" "1/4" "1/3" "1/2" "1" "3/2")
+list_nu=("0" "5" "10" "15" "20" "25" "30" "35")
+
+for beta in "${list_beta[@]}"; do
+  for nu in "${list_nu[@]}"; do
+    for i in $(seq 1 $n_sim); do
+      echo "Launching simulation $i using beta = $beta and nu = $nu"
+      sbatch --qos=fast --job-name="sim_${beta}_${nu}_${i}" --export=ALL,beta=$beta,nu=$nu,sim_id=$i run_simulation_intervention.sh
+    done
+  done
+done
---
+++ R/nodscov2/run_simulation_intervention.R
@@ -0,0 +1,57 @@
+library(Rcpp)
+library(dplyr)
+
+wd <- getwd()
+
+args <- commandArgs(trailingOnly = TRUE)
+
+load(file.path("/pasteur/appa/homes/ogaufres/intervention-analysis/RData/", 'parameters-admission-nodscov2.RData'))
+sourceCpp("/pasteur/appa/homes/ogaufres/intervention-analysis/cpp/dev-sensibility-analysis.cpp")
+
+## EDIT BETA AND NU
+sim_id <- as.character(args[1])
+beta_type <- gsub(pattern = '/', replacement = '-', x = as.character(args[2]))
+nu_type <- gsub(pattern = '/', replacement = '-', x = as.character(args[3]))
+
+beta <- eval(parse(text = args[2]))
+nu <- eval(parse(text = args[3]))
+
+## RANDOM INDEX CASE
+id_index <- sample(x= admission_sim %>% filter(id %in% admission$id) %>% distinct(id) %>% pull(), size = 1)
+# Update global status (index is infectious when the simulation starts)
+global_status <- global_status %>%
+  mutate(t_inf = ifelse(id == id_index,
+                        as.integer(1),
+                        t_inf),
+         t_incub = ifelse(id == id_index,
+                          as.integer(1),
+                          t_recover),
+         t_recover = ifelse(id == id_index,
+                            as.integer((t_incub)  + runif(1, min = 2880*3, max = 2880*7)),
+                            t_recover),
+         inf_by = ifelse(id == id_index,
+                         "INDEX",
+                         inf_by))
+
+## SIMULATION USING RCPP
+result <- simulation(global_interaction = global_interaction,
+                     global_environment = global_environment,
+                     global_data = global_data,
+                     global_status = global_status,
+                     beta = beta,
+                     B = B,
+                     nu = nu,
+                     mu = mu,
+                     env_threshold = env_threshold,
+                     dt = dt)
+
+
+## assign file name
+assign(paste0("sim_", beta_type, '_', nu_type, "_",  sim_id), result)
+save_path <- file.path(wd, "out", 'intervention-analysis', 'out', paste0('sim_', beta_type, '_', nu_type ), 'results')
+dir.create(save_path, recursive = T, showWarnings = F)
+
+# Save simulation results
+save(list = paste0("sim_", beta_type, '_', nu_type, "_",  sim_id), file = file.path(save_path, paste0('sim_', beta_type, '_', nu_type, '_', sim_id, ".RData")))
+
+
